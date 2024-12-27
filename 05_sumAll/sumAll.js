bb9dd1cdc1385d006367763fae777f1d354bc2fe
Parker Turner <parkerturnerpb@gmail.com>
2024-12-26 16:03:27 -0800

    Complete functions for exercises 01-06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    let result = 0;
+    // 1. Check if inputs are actually numbers
+    if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+        return "ERROR";
+    }
+    
+    // 2. Check if numbers are integers by comparing the number to its floor value
+    if (Math.floor(number1) !== number1 || Math.floor(number2) !== number2) {
+        return "ERROR";
+    }
+    
+    // 3. Check for negative numbers
+    if (number1 < 0 || number2 < 0) {
+        return "ERROR";
+    }
+    
+    if (number1 > number2) {
+        for (; number1 >= number2; number1--) {
+            result += number1;
+        }
+    }
+    else if (number1 < number2) {
+        for (; number1 <= number2; number1++) {
+            result += number1; 
+        }
+    }
+    return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(inputYear) {
+    if (inputYear % 100 === 0 && inputYear % 400 !== 0) {
+        return false
+    }
+    else if(inputYear % 4 === 0) {
+        return true
+    }
+    else return false
 };
 
 // Do not edit below this line
