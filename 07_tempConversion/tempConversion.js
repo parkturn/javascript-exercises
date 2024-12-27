c96b9a448acb461c019b3b9ee0d655f285fddf5c
Parker Turner <parkerturnerpb@gmail.com>
2024-12-26 16:28:52 -0800

    Passed spec test for 06 ecercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(temp) {
+    celciusTemp = ((temp - 32) * (5/9));
+    return Number(celciusTemp.toFixed(1));
+}
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(temp) {
+  fahrenheitTemp = (temp * (9/5) + 32);
+  return Number(fahrenheitTemp.toFixed(1));
 };
 
 // Do not edit below this line
