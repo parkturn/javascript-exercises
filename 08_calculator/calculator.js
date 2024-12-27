a522258913b4ad5ff28e8c1ec8876ee5087bc8f9
Parker Turner <parkerturnerpb@gmail.com>
2024-12-27 07:58:18 -0800

    Create a set of calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return Number(num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return Number(num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const initialValue = 0;
+  const sumWithInitial = array.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue,
+);
+  return sumWithInitial;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for (let i = 0; i < array.length; i++) {
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  poweredProduct = Number(num1**num2);
+  return poweredProduct;
 };
 
 const factorial = function() {
